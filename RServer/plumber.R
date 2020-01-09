e30eb605e03bf5e0983dc10f6aa4cd2d200aabf5
Bluesky19930906 <sblue5807276@gmail.com>
2020-01-09 10:48:27 -0800

    fix evt

--- RServer/plumber.R
+++ RServer/plumber.R
@@ -32,7 +32,6 @@ cors <- function(res) {
 #* @param camera -> camera number
 #* @get /fvis
 fvis <-function(from, to, zone, area, camera){
-  
   # dcpref value fields
   db_node_name = db_name
   op_val = 22
@@ -144,7 +143,7 @@ evt <- function(from, to, zone, area, camera){
   
   i_cab32 = i_zone * (2 ^ 26) + i_area * (2 ^ 20) + i_pad1 * (2 ^ 12) + i_camera * (2 ^ 6) + i_pad2
   
-  res_query <- dbGetQuery(data_handler, sprintf("select * from dc where (op16 = 20 AND cab32 = %d AND tick32 <= %d AND etick32 >= %d )", i_cab32, i_from, i_to))
+  res_query <- dbGetQuery(data_handler, sprintf("select * from dc where (op16 = 20 AND cab32 = %d AND tick32 >= %d AND etick32 <= %d ) order by tick32 asc", i_cab32, i_from, i_to))
   return_json_data = res_query
   
   cab32_val = gsub(' ', '', paste(paste(paste(paste(paste(paste(paste(paste(toString(i_zone), '-'), toString(i_area)), '-'), '0'), '-'), toString(i_camera)), '-'), '0'))
@@ -168,9 +167,6 @@ evt <- function(from, to, zone, area, camera){
   
   cab64_val = gsub(' ', '', paste(paste(paste(paste(paste(paste(paste(paste(paste(paste(toString(val1), '-'), toString(val2)), '-'), toString(val3)), '-'), toString(val4)), '-'), toString(val5)), '-'), toString(val6)))
   
-  dck_dcv_items = ''
-  calc_count = 0
-  
   list_op16 = return_json_data['op16']$op16
   list_cla16 = return_json_data['cla16']$cla16
   list_cab64s = cab64_val
@@ -178,48 +174,40 @@ evt <- function(from, to, zone, area, camera){
   list_id64 = return_json_data['id64']$id64
   list_id32 = return_json_data['id32']$id32
   list_dcv = return_json_data['jstr60k']$jstr60k
+  list_tick32 = return_json_data['tick32']$tick32
+  list_etick32 = return_json_data['etick32']$etick32
   
+  tick32_tag = list_tick32[[1]]
+  time_array_length = lengths(return_json_data['tick32'])
   
-  repeat {
-    calc_count = calc_count + 1
-    tmp_dck_dcv = ''
-    if (calc_count > lengths(return_json_data['cab64'])) {
-      break
-    } else if (calc_count == lengths(return_json_data['cab64'])) {
-      tmp_dck_dcv = sprintf('{"dck": {"op16": %d, "cla": %d, "cab64s": "%s", "cab32s": "%s", "id64": %s, "id32": %d}, "dcv": %s}',
-                            20,
-                            as.numeric(list_cla16[[calc_count]]),
-                            list_cab64s,
-                            list_cab32s,
-                            as.bigz(list_id64[[calc_count]]),
-                            list_id32[[calc_count]],
-                            list_dcv[[calc_count]]
-      )
+  json_dck_dcv = ''
+  json_all_dcpref = ''
+  json_result = ''
+  
+  calc_count = 1
+  
+  while (calc_count < (time_array_length + 1)) {
+    if (tick32_tag == list_tick32[[calc_count]]) {
+      dck_dcv_step_json = sprintf('{"dck": {"op16": %d, "cla": %d, "cab64s": "%s", "cab32s": "%s", "id64": %s, "id32": %d}, "dcv": %s},', 12, as.numeric(list_cla16[[calc_count]]), list_cab64s, list_cab32s, as.bigz(list_id64[[calc_count]]), list_id32[[calc_count]], list_dcv[[calc_count]])
+      json_dck_dcv = paste(json_dck_dcv, dck_dcv_step_json)
+      calc_count = calc_count + 1
     } else {
-      tmp_dck_dcv = sprintf('{"dck": {"op16": %d, "cla": %d, "cab64s": "%s", "cab32s": "%s", "id64": %s, "id32": %d}, "dcv": %s},',
-                            20,
-                            as.numeric(list_cla16[[calc_count]]),
-                            list_cab64s,
-                            list_cab32s,
-                            as.bigz(list_id64[[calc_count]]),
-                            list_id32[[calc_count]],
-                            list_dcv[[calc_count]]
-      )
+      dcpref_step_json = sprintf('{"dcpref": {"cab64s": "%s", "op": %d, "ser": %d}, "items":[%s]},', db_node_name, as.numeric(op_val), as.numeric(ser_val), json_dck_dcv)
+      json_all_dcpref = paste(json_all_dcpref, dcpref_step_json)
+      tick32_tag = list_tick32[[calc_count]]
     }
-    dck_dcv_items = paste(dck_dcv_items, tmp_dck_dcv)
   }
   
-  result_json = sprintf('{"dcpref": {"cab64s": "%s", "op": %d, "ser": %d}, "items":[%s]}',
-                        db_node_name,
-                        as.numeric(op_val),
-                        as.numeric(ser_val),
-                        dck_dcv_items)
   dbDisconnect(data_handler)
-  str_json = gsub("\\\\", "", jsonlite::toJSON(result_json))
-  str_prior_parse = sub('^[^\\{]*\\{', '{', str_json)
-  str_after_parse = gsub("\"]", "", str_prior_parse)
-  base64_enc_str = base64_enc(str_after_parse)
+  
+  str_json = gsub("\\\\", "", jsonlite::toJSON(json_all_dcpref))
+  str_prior_parse = sub('^[^\\{]*\\{', '[{', str_json)
+  str_after_parse = gsub("\"]", "]", str_prior_parse)
+  json_result = gsub("}\\,]", "}]", str_after_parse)
+  
+  base64_enc_str = base64_enc(json_result)
   return(base64_enc_str)
+  
 }
 
 #* Get EMeta Camera from Database
