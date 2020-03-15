fc52bd599c21eb1beb7373deebc19d31ffe02241
Bluesky19930906 <sblue5807276@gmail.com>
2020-03-15 16:02:46 -0700

    add chart component on filter

--- R-AccidentDashboard/app.R
+++ R-AccidentDashboard/app.R
@@ -3,12 +3,15 @@ library(leaflet)
 pos_lat <- c(31.497555, 31.513837, 31.474463, 31.465495)
 pos_lon <- c(120.317609, 120.356662, 120.356491, 120.348036)
 
+chartTitle <- c("Crash", "Accident", "Injury")
+chartValue <- c(16, 13, 29)
 
 server <- function(input, output, session) {
   
   #place leaflet map in html component
   observe({
     session$sendCustomMessage("sendCameraPosition", c(pos_lat, pos_lon))
+    session$sendCustomMessage("sendChartData", c(chartTitle, chartValue))
   })
   
 }
